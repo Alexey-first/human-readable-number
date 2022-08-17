861e7b79ddf786822fb002e4020645b0fc3e93d3
Alexey-first <55013000a@gmail.com>
2022-08-17 17:16:43 +0300

    feat: start task and implement one part

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function toReadable (number) {
-  
+  let str = String(number);
+  let n = str.length;  
+    for (let i = 0; i < n; i++) {
+    if (n === 1) {
+        return oneItem(str[0]);
+     }    
+  } 
+ }
+
+
+function oneItem (item) {
+    switch (item) {
+        case '0': return 'zero';
+            break;
+        case '1': return 'one';
+            break;
+        case '2': return 'two';
+            break;
+        case '3': return 'three';
+            break;
+        case '4': return 'four';
+            break;
+        case '5': return 'five';
+            break;
+        case '6': return 'six';
+            break;
+        case '7': return 'seven';
+            break;
+        case '8': return 'eight';
+            break;
+        case '9': return 'nine';
+            break;
+    }
 }
+console.log(toReadable(7));
