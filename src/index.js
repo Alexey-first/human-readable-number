aeaa4a0132ddd0045e97f4aabfe875ef8b3b6aec
Alexey-first <55013000a@gmail.com>
2022-08-17 21:59:48 +0300

    feat implement task

--- src/index.js
+++ src/index.js
@@ -4,8 +4,26 @@ module.exports = function toReadable (number) {
     for (let i = 0; i < n; i++) {
     if (n === 1) {
         return oneItem(str[0]);
-     }    
-  } 
+     } else if(n === 2) {
+        if(str[0] === '1') {
+            return twoItem1(str[1]);
+         } else if(str[0] !== '1' && str[1] === '0') {
+             return twoItem2(str[0]);
+         } else if(str[0] !== '1' && str[1] !== '0') {
+            return `${twoItem2(str[0])} ${oneItem(str[1])}`
+         }
+     } else if (n === 3) {
+        if(str[1] === '0' && str[2] === '0') {
+            return `${oneItem(str[0])} hundred`;
+         } else if(str[1] === '1') {
+             return `${oneItem(str[0])} hundred ${twoItem1(str[2])}`
+         } else if(str[1] !== '1' && str[2] === '0') {
+            return `${oneItem(str[0])} hundred ${twoItem2(str[1])}`
+         } else if(str[1] !== '1' && str[2] !== '0') {
+            return `${oneItem(str[0])} hundred ${twoItem2(str[1])} ${oneItem(str[1])}`
+         }
+     }       
+  }  
  }
 
 
@@ -33,4 +51,51 @@ function oneItem (item) {
             break;
     }
 }
-console.log(toReadable(7));
+
+function twoItem1 (item) {
+    switch(item) {
+        case '0': return 'ten';
+        break;
+        case '1': return 'eleven';
+        break;
+        case '2': return 'twelve';
+        break;
+        case '3': return 'thirteen';
+        break;
+        case '4': return 'fourteen';
+        break;
+        case '5': return 'fifteen';
+        break;
+        case '6': return 'sixteen';
+        break;
+        case '7': return 'seventeen';
+        break;
+        case '8': return 'eighteen';
+        break;
+        case '9':  return 'nineteen';
+        break;
+    }
+}
+
+function twoItem2 (item) {
+    switch(item) {
+        case '2': return 'twenty';
+        break;
+        case '3': return 'thirty';
+        break;
+        case '4': return 'fourty';
+        break;
+        case '5': return 'fifty';
+        break;
+        case '6': return 'sixty'
+        break;
+        case '7': return 'seventy';
+        break;
+        case '8': return 'eighty';
+        break;
+        case '9':  return 'ninety';
+        break;
+    }
+}
+
+console.log(toReadable(6));
